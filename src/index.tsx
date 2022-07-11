6bc6d938dfca1709102462ad01ecef5978fee4e4
TimoxaTZ <t1m0xa067@gmail.com>
2022-07-11 21:22:49 +0300

    refactoring react v18

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+// import { createRoot } from 'react-dom/client'
 import { App } from './app/entry'
 
-const container = document.getElementById('root');
+// const container = document.getElementById('root')
+// const root = createRoot(container)
+// root.render(<App.Container />, container)
 
-
-ReactDOM.render(<App.Container />, container)
+import ReactDOM from 'react-dom' //react v17
+const container = document.getElementById('root') //react v17
+ReactDOM.render(<App.Container />, container) //react v17
